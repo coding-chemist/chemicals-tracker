9b6c59e2488569494ac9402e6ca15c37b62a00f1
Sindhuja Sivaraman <93024102+coding-chemist@users.noreply.github.com>
2024-06-10 12:25:28 +0530

    Add stubs for Chemicals, Inventory, and Utils classes

---
+++ code/utils/utils.py
@@ -0,0 +1,30 @@
+# utils.py
+
+class UtilityFunctions():
+    """Represents Utils Functions"""
+    def __init__(self) -> None:
+        pass
+
+    def _generate_unique_id():
+        pass
+
+    def validate_name():
+        pass
+
+    def validate_cas_number():
+        pass
+
+    def validate_product_number():
+        pass
+
+    def validate_quantity():
+        pass
+
+    def validate_location():
+        pass
+
+    def classify_hazard_category():
+        pass
+
+    def convert_unit():
+        pass
